78652e116079641436496d8a604ab18733e644c3
lucianopf <luciano.franca@pagar.me>
2023-09-21 18:53:46 -0300

    slack: udgrade to slack/bolt

--- index.js
+++ index.js
@@ -1,48 +1,58 @@
 require('dotenv').config()
-const SmartSlack = require('smartslack')
-
+const { App } = require('@slack/bolt')
 const { makeQuestion } = require('./lib/chatgpt')
-const { sendMessage } = require('./lib/slack')
 
 const {
   SLACKBOT_TOKEN,
+  SLACKBOT_APP_TOKEN,
   SLACKBOT_USER_ID,
 } = process.env  
 
-const smartSlackClient = new SmartSlack({ token: SLACKBOT_TOKEN })
- 
-smartSlackClient.on('error', (error) => console.log(error))
-  
-smartSlackClient.on('connected', () => console.log('Bot connected and watching for messages.'))
-  
-smartSlackClient.on('message', async (message) => {
-  if (
-    message.type === 'message' &&
-    message.text &&
-    message.text.includes(`<@${SLACKBOT_USER_ID}>`) &&
-    message.subtype !== 'channel_join') {
-    console.log(message)
-
-    let user = message.ts
-
-    if (message.thread_ts) {
-      user = message.thread_tsx
-    }
-
-    const response = await makeQuestion(
-      message.text.replace(`<@${SLACKBOT_USER_ID}>`, ''),
-      user
-    )
-
-    console.log(JSON.stringify(response))
-
-    const { message: responseMessage } = response.choices.pop()  
-    const { content } = responseMessage
-
-    const slackresponse = await sendMessage(content, message.channel, message.ts)
-
-    console.log(slackresponse)
-  }
+const app = new App({
+  token: SLACKBOT_TOKEN, 
+  appToken: SLACKBOT_APP_TOKEN,
+  socketMode: true,
 })
 
-smartSlackClient.start()
+async function main () {
+  await app.start();
+  console.log('⚡️ Bolt app started')
+
+  app.message(async ({ message, say }) => {
+    if (
+      message.type === 'message' &&
+      message.text &&
+      message.text.includes(`<@${SLACKBOT_USER_ID}>`) &&
+      message.subtype !== 'channel_join'
+    ) {
+      try {
+        console.log(JSON.stringify(message))
+  
+        let user = message.ts
+    
+        if (message.thread_ts) {
+          user = message.thread_tsx
+        }
+    
+        const response = await makeQuestion(
+          message.text.replace(`<@${SLACKBOT_USER_ID}>`, ''),
+          user
+        )
+    
+        console.log(JSON.stringify(response))
+    
+        const { message: responseMessage } = response.choices.pop()  
+        const { content } = responseMessage
+  
+        await say({
+          text: content,
+          thread_ts: message.ts,
+        })
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  })
+}
+
+main()
